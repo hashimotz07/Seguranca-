2d24197933ffcd037def1601efe60daec87feb64
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-04-08 17:32:53 -0300

    criacao da pasta cifras

---
+++ cifras/transposicao.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> //biblioteca de programação competitiva
+#define int long long
+#define pb push_back
+#define inacio ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+using namespace std;
+
+int32_t main()
+{
+    inacio //tira o delay do cin e do cout
+
+    string chave = "supinoreto";
+    string texto = "trembolona";
+
+
+
+    return 0;
+}
